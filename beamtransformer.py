55ba4612ab3e6f1056e6b1d6aefb1d68353c1df8
Wilmer E. Henao <wilmer@umich.edu>
2018-01-20 23:55:15 -0500

    transform beam data to a simpler format

---
+++ beamtransformer.py
@@ -0,0 +1,49 @@
+import pickle
+from collections import defaultdict
+
+myfolder = '/mnt/datadrive/Dropbox/Data/spine360/by-Beam/'
+#myfolder = '/home/wilmer/Dropbox/Data/spine360/by-Beam/'
+#data = pickle.load( open( myfolder + "358.pickle", "rb" ) )
+
+
+
+class beamletdose:
+    def __init__(self, a):
+        self.Index = a.Index
+        self.Dose = a.Dose
+
+
+def load(filename):
+    with open(filename, "rb") as f:
+        while True:
+            try:
+                yield pickle.load(f)
+            except EOFError:
+                break
+
+for mynum in range(0,359,2):
+    PIK = myfolder + str(mynum) + ".pickle"
+    items = load(PIK)
+    di = defaultdict(list) #indices
+    dd = defaultdict(list) #doses
+    for itemlist in items:
+        for dp in itemlist:
+            di[dp[0]].append(dp[1].Index)
+            dd[dp[0]].append(dp[1].Dose)
+    output = open(myfolder + 'twolists' + str(mynum) + '.pickle', 'ab')
+    pickle.dump([di,dd], output)
+    output.close()
+    d = None
+
+for mynum in range(0,359,2):
+    PIK = myfolder + str(mynum) + ".pickle"
+    items = load(PIK)
+    d = defaultdict(list)
+    for itemlist in items:
+        for dp in itemlist:
+            d[dp[0]].append(beamletdose(dp[1]))
+    output = open(myfolder + 'defaultdict' + str(mynum) + '.pickle', 'ab')
+    pickle.dump(d, output)
+    output.close()
+    d = None
+#myitem = next(items)
